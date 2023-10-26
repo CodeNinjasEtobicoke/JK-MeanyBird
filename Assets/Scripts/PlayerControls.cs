53c651462ce168ae89428ac52abf56a6094975e3
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-10-26 17:59:47 -0400

    MeanyBird

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -9,17 +9,26 @@ public class PlayerControls : MonoBehaviour
     [Header("Default Velocity")]
     public float velocity = 1;
     private Rigidbody2D rb;
+    private float objectHieght;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        gameController = GetComponent<GameController>();
+        Time.timeScale = 1;
+        rb = GetComponent<Rigidbody2D>();
+        objectHieght = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+            rb.velocity = Vector2.up * velocity;
+
 
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("Spikes Object for controlling the game")]
+    public GameObject spikes;
+    [Header("Default Height")]
+    public float height;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("InstantiateObjects")
     }
 
     // Update is called once per frame
