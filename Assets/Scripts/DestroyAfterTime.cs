16e1e132898e34cf73b67a121779825f7cb36995
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-11-10 17:34:13 -0500

    joshua

--- Assets/Scripts/DestroyAfterTime.cs
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyAfterTime : MonoBehaviour
 {
+    [Header("Default Destruction Time")]
+    public float timeToDestruction;
     // Start is called before the first frame update
     void Start()
     {
-
+        Invoke("DestroyObject", timeToDestruction);
     }
 
     // Update is called once per frame
