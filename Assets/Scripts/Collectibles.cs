1767e3e963c5b4dc777b49ba2d8c8548c9b8916d
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-12-06 18:52:21 -0500

    j

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -7,15 +7,13 @@ public class Collectibles : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "HighSpike" || collision.gameObject.tag == "LowSpike" || collision.gameObject.tag == "Ground")
-        {
-            Time.timeScale = 0;
-        }
+        Score.score++;
     }
+}
 
     // Update is called once per frame
 
-}
+
 
     
 
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -31,4 +31,11 @@ public class PlayerControls : MonoBehaviour
 
 
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "HighSpike" || collision.gameObject.tag == "LowSpike" || collision.gameObject.tag == "Ground")
+        Time.timeScale = 0;
+
+        GameObject.Find("GameControler").GetComponent<GameController>().GameOver();
+
 }
